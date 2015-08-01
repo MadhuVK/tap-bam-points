9303ef92c07c252c9b3916dec3875fb21739b46c
Madhu Krishnan <madhuv.krishnan@gmail.com>
2015-08-01 15:27:48 -0700

    DB Mapping

---
+++ tBp-Web/lib/dbhelper.js
@@ -0,0 +1,23 @@
+//TODO: Build up from metadata
+
+var dbMap = {
+    "tbp" : {
+        "user": {
+            "house_color": "tbp_user",
+            "member_status": "tbp_user",
+            "last_name": "user",
+            "first_name": "user",
+            "barcode_hash": "user"
+        },
+        "event": {
+            "event_type": "tbp_event",
+            "officer": "tbp_event",
+            "default_points": "tbp_event",
+            "datetime": "event",
+            "name": "event",
+            "valid": "event"
+        }
+    }
+};
+
+
