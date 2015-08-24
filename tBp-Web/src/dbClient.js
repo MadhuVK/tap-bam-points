39ac3ba131b4fc89bae3b24aac189d7d7ee86e6f
Madhu Krishnan <madhuv.krishnan@gmail.com>
2015-08-23 18:18:09 -0700

    Merge

--- tBp-Web/src/dbClient.js
+++ tBp-Web/src/dbClient.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
-//TODO: Build up from metadata
 
+//TODO: Build up from metadata
+// May not need this anymore
 var dbMap = {
     "tbp" : {
         "user": {
@@ -21,23 +22,79 @@ var dbMap = {
     }
 };
 
-var connection = mysql.createConnection({
+var totalConnections = 0;
+var totalEnqueues = 0;
+
+var pool = mysql.createPool({
+    connectionLimit: 50,
     host: "localhost",
     user: "root",
     password: "",
     database: 'tBp'
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("Begin error");
-        console.error("error connection: " + err.stack);
-        console.error("End error");
-        return;
-    }
-
-    console.log("dbClient: connected to localhost mysql");
+pool.on('connection', function(connection) {
+    console.log('Total Connections Available: ' + ++totalConnections);
+    console.log('Connection id: ' + connection.threadId);
 });
 
-module.exports = connection;
+pool.on('enqueue', function() {
+    console.log('Total Enqueues' + ++totalEnqueues);
+});
 
+/* USAGE: TODO
+ */
+
+exports.db_select = function(columns, tables, conditions) {
+    console.log("Issuing SELECT");
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.error("Error on establishing connection");
+            throw err;
+        }
+
+        connection.release();
+    });
+
+    return {};
+};
+
+exports.db_insert = function() {
+    console.log("Issuing INSERT");
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.error("Error on establishing connection");
+            throw err;
+        }
+        connection.release();
+    });
+
+    return {};
+};
+
+exports.db_update = function() {
+    console.log("Issuing UPDATE");
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.error("Error on Establishing connection");
+            throw err;
+        }
+        connection.release();
+    });
+
+    return {};
+};
+
+// We shouldn't have to use this, since we're doing lazy deletion
+exports.db_delete = function() {
+    console.log("Issuing DELETE");
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.error("Error on Establishing connection");
+            throw err;
+        }
+        connection.release();
+    });
+
+    return {};
+};
