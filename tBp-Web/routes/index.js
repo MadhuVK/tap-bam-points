da889642c058d9f56c10a984598060dc21921cb2
Spencer Wilson <sw12@users.noreply.github.com>
2015-08-29 17:46:03 -0700

    Added rough user page at /me

--- tBp-Web/routes/index.js
+++ tBp-Web/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../src/data.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+router.get('/me', function(req, res) {
+  const USER = 1;
+  data.getUserById(USER, function(user) {
+    data.getUserAttendanceHistory(USER, function(history) {
+      user.history = history;
+      res.render('user', { title: 'Your TBP profile', user: user });
+    });
+  });
 });
 
 module.exports = router;
