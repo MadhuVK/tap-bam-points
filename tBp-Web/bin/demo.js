74634a01717dc9d96fde6d51951c43ef7b582b52
Spencer Wilson <sw12@users.noreply.github.com>
2016-01-22 00:32:32 -0800

    Promisify demo data insertion

---
+++ tBp-Web/bin/demo.js
@@ -0,0 +1,6 @@
+exports.users = [
+  { "firstName": "Antonio", "lastName": "Ngonye", "barcodeHash": "123", "house": "red", "memberStatus": "initiate" },
+  { "firstName": "Mel", "lastName": "D", "barcodeHash": "456", "house": "blue", "memberStatus": "member" },
+  { "firstName": "Mad", "lastName": "Brown Dog", "barcodeHash": "789", "house": "red", "memberStatus": "member" },
+  { "firstName": "Spencer", "lastName": "Beard", "barcodeHash": "090", "house": "green", "memberStatus": "officer" }
+];
