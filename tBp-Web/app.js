eaa92ad0e74a944787c98b9ad35ea73a398298e1
Jesse Qin <itzjesse@gmail.com>
2016-02-08 06:37:10 -0800

    initial styling skeleton starting to be laid out

--- tBp-Web/app.js
+++ tBp-Web/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
 
 var baseRoutes = require('./routes/index');
 var userRoutes = require('./routes/users');
@@ -11,52 +12,63 @@ var eventRoutes = require('./routes/events');
 var adminRoutes = require('./routes/admin');
 var errorRoutes = require('./routes/errors');
 
-var config = require('./bin/config')[process.env.NODE_ENV]
+var config = require('./bin/config')[process.env.NODE_ENV];
 
 var app = express();
-var session = require('express-session'); 
-var RedisStore = require('connect-redis')(session); 
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public/stylesheets'),
+    dest: path.join(__dirname, 'public/stylesheets'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/stylesheets', // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function baseSetup() {
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
 
-  app.use(cookieParser());
-  app.use(session({
-    store: new RedisStore({
-      host: 'localhost', 
-      port: 6379,
-      db: 2, 
-      pass: "jubyjuby22"
-    }), 
-    secret: "MY_SECRET_KEY"
-  })); 
+    app.use(cookieParser());
+    app.use(session({
+        store: new RedisStore({
+            host: 'localhost',
+            port: 6379,
+            db: 2,
+            pass: "jubyjuby22"
+        }),
+        secret: "MY_SECRET_KEY"
+    }));
 
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.set('json spaces', 2);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set('json spaces', 2);
 }
 
 function viewEngineSetup() {
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
-  app.engine('html', require('ejs').__express); 
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.engine('html', require('ejs').__express);
 }
 
 
 
 function routesSetup() {
-  app.use(errorRoutes);
-  app.use('/', baseRoutes);
-  app.use('/admin', adminRoutes); 
-  app.use('/api', apiBaseRoutes); 
-  app.use('/api/users', userRoutes);
-  app.use('/api/events', eventRoutes);
+    app.use(errorRoutes);
+    app.use('/', baseRoutes);
+    app.use('/admin', adminRoutes);
+    app.use('/api', apiBaseRoutes);
+    app.use('/api/users', userRoutes);
+    app.use('/api/events', eventRoutes);
 }
 
 baseSetup();
 viewEngineSetup();
 routesSetup();
 
-app.config = config; 
+app.config = config;
 module.exports = app;
